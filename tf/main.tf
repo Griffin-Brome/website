708ecc24975594cb9f73e2b3353fa8e8bcfb0bb8
Griffin <griffinbrome@gmail.com>
2024-08-11 19:37:30 -0700

    Format

--- tf/main.tf
+++ tf/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
@@ -12,29 +12,29 @@ provider "digitalocean" {
 }
 
 resource "digitalocean_ssh_key" "terraform" {
-  name = "terraform"
+  name       = "terraform"
   public_key = file("${var.ssh_key}.pub")
 }
 
 resource "digitalocean_droplet" "www" {
-  image     = "ubuntu-24-04-x64"
-  name      = "www"
-  region    = var.region
-  size      = "s-1vcpu-2gb"
+  image  = "ubuntu-24-04-x64"
+  name   = "www"
+  region = var.region
+  size   = "s-1vcpu-2gb"
   user_data = templatefile(
     "${path.module}/templates/user-data.tftpl",
     {
-      certbot_do_token  = var.certbot_do_token,
-      ssl_renew_email   = var.ssl_renew_email,
-      domain_name       = var.domain_name
+      certbot_do_token = var.certbot_do_token,
+      ssl_renew_email  = var.ssl_renew_email,
+      domain_name      = var.domain_name
     }
   )
-  ssh_keys  = [
-      digitalocean_ssh_key.terraform.id
+  ssh_keys = [
+    digitalocean_ssh_key.terraform.id
   ]
 }
 
 resource "digitalocean_domain" "www" {
   name       = var.domain_name
-  ip_address = digitalocean_droplet.www.ipv4_address 
+  ip_address = digitalocean_droplet.www.ipv4_address
 }
