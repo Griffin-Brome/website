e4c7c4863a91204f3dd5377b91dde04afbfc9672
Griffin <griffinbrome@gmail.com>
2024-08-11 19:15:58 -0700

    Configure Certbot

--- droplets.tf
+++ droplets.tf
@@ -1,16 +1,24 @@
-data "template_file" "user_data" {
-  template = file("./user-data")
-}
-
 resource "digitalocean_droplet" "www" {
   image     = "ubuntu-24-04-x64"
   name      = "www"
   region    = var.region
   size      = "s-1vcpu-2gb"
-  user_data = data.template_file.user_data.rendered
+  user_data = templatefile(
+    "${path.module}/templates/user-data.tftpl",
+    {
+      certbot_do_token  = var.certbot_do_token,
+      ssl_renew_email   = var.ssl_renew_email,
+      domain_name       = var.domain_name
+    }
+  )
   ssh_keys  = [
       digitalocean_ssh_key.terraform.id
   ]
+}
+
+resource "digitalocean_domain" "www" {
+  name       = var.domain_name
+  ip_address = digitalocean_droplet.www.ipv4_address 
 }
 
 output "public_ip" {
--- variables.tf
+++ variables.tf
@@ -11,3 +11,15 @@ variable "region" {
   description = "Digital Ocean Region"
   default     = "sfo1"
 }
+
+variable "certbot_do_token" {
+  description = "Digital Ocean API Token to use for DNS-01 Challenges"
+}
+
+variable "domain_name" {
+  description = "Site domain name"
+}
+
+variable "ssl_renew_email" {
+  description = "Email that Certbot will use for SSL Renewal Alerts"
+}
