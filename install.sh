ec57ebe1dc27f780bd27ba7bdf66ded52b08d958
Griffin Brome <6681097+Griffin-Brome@users.noreply.github.com>
2024-12-23 12:11:02 -0800

    chore(install): don't bother w/ git

--- install.sh
+++ install.sh
@@ -4,42 +4,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-
-function __setup_git {
-    echo 'Creating new git credentials.'
-    local git_username=$1
-    local git_email=$2
-    read -p "What name would you like to use for git? " git_username 
-    read -p "What email would you like to use for git? " git_email
-    echo "export GIT_AUTHOR_NAME=$git_username" >> $HOME/.zshenv.local
-    echo "export GIT_AUTHOR_EMAIL=$git_email" >> $HOME/.zshenv.local
-    echo "Git credentials saved to $HOME/.zshenv.local"
-    echo "Source it for them to take effect"
-}
-
-
 function main {
     echo 'Setting up a new machine'
-    echo 'Checking for existing git credentials'
-    if [ ! -z ${GIT_AUTHOR_NAME} ] && [ ! -z ${GIT_AUTHOR_EMAIL} ]; then
-        echo 'Git credentials already found'
-    else
-        echo 'No git credentials found' 
-        while true; do
-            local response
-            read -p 'Set them up now? (Y/n): ' response
-            case "${response:-n}" in
-                [Yy]) __setup_git; break ;;
-                [Nn]) break ;;
-                *) echo "Invalid choice" ;;
-            esac
-        done
-    fi
-
     ssh_key_name="$HOME/.ssh/id_ed25519"
     if [ ! -e $ssh_key_name ]; then
         echo 'Creating an SSH keypair...'
-        ssh-keygen -t ed25519 -C $GIT_USER_EMAIL -f $ssh_key_name -N ''
+	ssh-keygen -t ed25519 -C "$(git config user.email)" -f $ssh_key_name -N ''
         echo "Generated a new SSH key: $ssh_key_name"
         eval "$(ssh-agent -s)"
         ssh-add $ssh_key_name
